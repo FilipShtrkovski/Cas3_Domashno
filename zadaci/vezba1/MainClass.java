de687ffb3a5593c21107e55a8ddf54ea1afd6768
FilipShtrkovski <filip.strkovski13@gmail.com>
2022-11-13 22:05:51 +0100

    Cas 3 zadaci

---
+++ zadaci/vezba1/MainClass.java
@@ -0,0 +1,12 @@
+package vezba1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Avtomobil av = new Avtomobil("Honda","Civic","Black",180000, 2007 , "BT-007-JB");
+				av.pMetod();
+				System.out.println(av.vMetod());
+
+	}
+
+}
---
+++ zadaci/vezba4/Brojac.java
@@ -0,0 +1,13 @@
+package vezba4;
+
+public class Brojac {
+	public int brojach;
+	
+	public void zgolemi () {
+		this.brojach++;
+	}
+	
+	public void reset () {
+		this.brojach = 0;
+	}
+}
